d8fcd52494250f400dad775ffd5d8b02804bd6f6
jennieyhkang <jennieyhkang@gmail.com>
2026-04-20 20:31:52 -0400

    Create UmamiAnalytics.js

---
+++ app/UmamiAnalytics.js
@@ -0,0 +1,16 @@
+js'use client'
+import Script from 'next/script'
+
+export const UmamiAnalytics = () => {
+  const websiteId = process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID
+  if (!websiteId) return null
+
+  return (
+    <Script
+      async
+      src="https://cloud.umami.is/script.js"
+      data-website-id={websiteId}
+      strategy="afterInteractive"
+    />
+  )
+}
